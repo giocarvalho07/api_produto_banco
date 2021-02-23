eea23cdc1f42986af0034fe8e8e6f17c5a2c5b14
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-02-23 00:15:01 -0300

    api produtos bancarios com springboot

---
+++ src/main/java/com/produto/model/Produto.java
@@ -0,0 +1,39 @@
+package com.produto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cod;
+	private String nome;
+	private Double preco;
+	
+	public Long getCod() {
+		return cod;
+	}
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
